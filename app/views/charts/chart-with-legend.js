6edf76c5b723aca81a3443ef3477c452d1bff20d
Kyungmin Kim <kmin905@gmail.com>
2014-06-19 15:16:07 -0700

    Add list table to the overview page

--- app/views/charts/chart-with-legend.js
+++ app/views/charts/chart-with-legend.js
@@ -1,11 +1,5 @@
 Balanced.ChartWithLegendView = Ember.View.extend({});
 
 Balanced.LineChartWithLegendView = Balanced.ChartWithLegendView.extend({
-	templateName: 'charts/line-chart-with-legend',
-	data: function() {
-		var controller = this.get('controller');
-		var chartName = this.get('chartName');
-
-		return controller.get(chartName);
-	}.property('controller', 'chartName')
+	templateName: 'charts/line-chart-with-legend'
 });
