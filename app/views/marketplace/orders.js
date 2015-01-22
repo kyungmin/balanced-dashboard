63adaa30fe59cce8227b33b542bc3bc7a800c3af
Kyungmin Kim <kmin905@gmail.com>
2015-01-22 15:23:09 -0800

    Improve the empty states

--- app/views/marketplace/orders.js
+++ app/views/marketplace/orders.js
@@ -1,7 +1,6 @@
 var MarketplaceOrdersView = Ember.View.extend({
 	layoutName: "marketplace/payments-layout",
 	templateName: "marketplace/orders",
-	hasResults: Ember.computed.gt("controller.resultsLoader.results.length", 0),
 });
 
 export default MarketplaceOrdersView;
